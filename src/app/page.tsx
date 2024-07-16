ba4c356987664f9b3f318d9ad7823c8a8fbed718
Kento <alan_gg2001@hotmail.com>
2024-07-16 01:15:53 -0700

    add clerk + supabase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,20 @@
+import Link from 'next/link';
+import { currentUser } from '@clerk/nextjs/server';
 
-
-export default function Page() {
+export default async function Page() {
+  const user = await currentUser();
+  console.log(user?.id);
 
   return (
-    <div className=''>
-      <h2>Landing</h2>
+    <div className='w-full flex flex-col items-center justify-center'>
+      {user &&
+        <Link href={'/dashboard'}>
+          <button className='bg-black p-4 rounded-sm flex items-center justify-center'>
+            <span className='text-lg text-white'>CLICK TO START</span>
+          </button>
+        </Link>
+      }
+
     </div>
   )
 }
